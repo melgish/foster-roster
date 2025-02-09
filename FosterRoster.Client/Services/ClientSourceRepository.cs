fa091fe95c8ecd3a36c6d14a50e721c931dbe4b4
Nick <melgish@users.noreply.github.com>
2025-02-09 11:57:15 -0500

    Add source admin (#11)
    * Add sources page
    * Add confirm
    * more responsive dashboard
    * bump version

--- FosterRoster.Client/Services/ClientSourceRepository.cs
+++ FosterRoster.Client/Services/ClientSourceRepository.cs
@@ -6,6 +6,7 @@ public sealed class ClientSourceRepository(
 {
     private const string Route = "api/sources";
     private const string FailedToCreate = "Failed to create source";
+    private const string FailedToDelete = "Failed to delete source";
 
     /// <summary>
     ///     Adds a new source
@@ -18,6 +19,17 @@ public sealed class ClientSourceRepository(
             .Bind(rs => Result.OkIf(rs.IsSuccessStatusCode, FailedToCreate).ToResult(rs))
             .Bind(rs => Result.Try(() => rs.Content.ReadFromJsonAsync<Source>()))
             .Bind(s => Result.OkIf(s is not null, FailedToCreate).ToResult(s!));
+
+    /// <summary>
+    ///     Deletes a Source by its ID.
+    /// </summary>
+    /// <param name="sourceId">ID of source to remove.</param>
+    /// <returns>A Result instance indicating success or failure.</returns>
+    public async Task<Result> DeleteByKeyAsync(int sourceId)
+        => await Result
+            .Try(() => httpClient.DeleteAsync($"{Route}/{sourceId}"))
+            .Bind(rs => Result.OkIf(rs.IsSuccessStatusCode, FailedToDelete));
+
 
     /// <summary>
     ///     Get list of all sources in the database.
@@ -36,4 +48,27 @@ public sealed class ClientSourceRepository(
         => await Result
             .Try(() => httpClient.GetFromJsonAsync<List<ListItem<int>>>($"{Route}/names"))
             .Bind((list) => Result.Ok(list!));
+
+    /// <summary>
+    ///     Gets single Source from the database.
+    /// </summary>
+    /// <param name="sourceId">ID of source to return.</param>
+    /// <returns>Result with Source if successful, or Errors on failure.</returns>
+    public async Task<Result<Source>> GetByKeyAsync(int sourceId)
+        => await Result
+            .Try(() => httpClient.GetFromJsonAsync<Source>($"{Route}/{sourceId}"))
+            .Bind(source => Result.Ok(source!));
+
+    /// <summary>
+    ///     Updates an existing Source in the database.
+    /// </summary>
+    /// <param name="sourceId">ID of Source to update</param>
+    /// <param name="source">Data to assign to Source</param>
+    /// <returns>Result with updated Source if found, or Errors on failure.</returns>
+    public async Task<Result<Source>> UpdateAsync(int sourceId, Source source)
+    {
+        var model = new SourceEditModel(source);
+        var rs = await httpClient.PutAsJsonAsync($"{Route}/{sourceId}", model);
+        return Result.Ok((await rs.Content.ReadFromJsonAsync<Source>())!);
+    }
 }
---
+++ FosterRoster.Domain/SourceEditModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FosterRoster.Domain;
+
+public sealed class SourceEditModel()
+{
+    public int Id { get; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public SourceEditModel(Source source) : this()
+    {
+        Id = source.Id;
+        Name = source.Name;
+    }
+
+    public Source ToSource() =>
+        new()
+        {
+            Id = Id,
+            Name = Name
+        };
+}
