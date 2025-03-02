6f03762ef15cc8e5758a80e804e9f1ee994e3c65
Nicholas J. Fiorello Jr. <nfiorello@yahoo.com>
2025-03-02 09:10:33 -0500

    update print view

---
+++ FosterRoster/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace FosterRoster.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static DateTimeOffset ToLocalTime(this DateTimeOffset dateTimeOffset, TimeProvider timeProvider)
+        => TimeZoneInfo.ConvertTime(dateTimeOffset, timeProvider.LocalTimeZone);
+
+    public static string FormatLocalTime(this DateTimeOffset dateTimeOffset, TimeProvider timeProvider,
+        string format = "g")
+        => ToLocalTime(dateTimeOffset, timeProvider).ToString(format);
+}
