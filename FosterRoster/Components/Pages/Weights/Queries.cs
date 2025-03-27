f4159e560b633887d42dcfe495e79f03df78c1f9
Nicholas J. Fiorello Jr. <nfiorello@yahoo.com>
2025-03-27 06:25:24 -0400

    in-process

---
+++ FosterRoster/Components/Pages/Weights/Queries.cs
@@ -0,0 +1,8 @@
+﻿namespace FosterRoster.Components.Pages.Weights;
+
+internal static class Queries
+{
+    public static IQueryable<Weight> ForFeline(this IQueryable<Weight> queryable, int felineId)
+        => felineId == 0 ? queryable : queryable.Where(w => w.FelineId == felineId);
+ 
+}
