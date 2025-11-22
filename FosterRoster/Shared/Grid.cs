2d2cdda868f1ca32a1c795152731e99c3c64f05d
melgish <nfiorello@yahoo.com>
2025-11-21 20:14:47 -0500

    Make constant

--- FosterRoster/Shared/Grid.cs
+++ FosterRoster/Shared/Grid.cs
@@ -7,11 +7,11 @@ using Radzen;
 /// </summary>
 public static class Grid
 {
-    public static readonly Density Density = Density.Compact;
-    public static readonly FilterCaseSensitivity FilterCaseSensitivity = FilterCaseSensitivity.CaseInsensitive;
+    public const Density Density = Radzen.Density.Compact;
+    public const FilterCaseSensitivity FilterCaseSensitivity = Radzen.FilterCaseSensitivity.CaseInsensitive;
     public const int PageNumbersCount = 1;
     public const HorizontalAlign PagerHorizontalAlign = HorizontalAlign.Center;
-    public static readonly PagerPosition PagerPosition = PagerPosition.Top;
+    public const PagerPosition PagerPosition = Radzen.PagerPosition.Top;
     public const int PageSize = 20;
     public static readonly int[] PageSizeOptions = [10, 20, 50, 100];
     public const string PagingSummaryFormat = "Showing {0} to {1} of {2}";
